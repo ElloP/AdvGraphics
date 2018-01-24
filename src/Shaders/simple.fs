9702e44d823181833c506105f86ba5e05b40e3ff
klas <klas@ludvigsson.se>
2018-01-24 17:22:41 +0100

    forgot to add the fragment shader

--- src/Shaders/simple.fs
+++ src/Shaders/simple.fs
@@ -1,7 +1,8 @@
 #version 420 core
 
 uniform vec4 inColor;
-layout (binding = 3) uniform sampler2D ourTexture;
+layout (binding = 0) uniform sampler2D ourTexture;
+layout (binding = 1) uniform sampler2D ourTexture2;
 
 in vec3 viewSpaceNormal;
 in vec3 viewSpacePosition;
@@ -12,12 +13,14 @@ out vec4 color;
 vec3 lightPos = vec3(1.2,0.0,2.0);
 vec3 lightColor = vec3(1.0,1.0,1.0);
 
+uniform float blend;
+
 vec3 ambient()
 {
     float ambientStrength = 0.1f;
     vec3 ambient = ambientStrength * lightColor;
 
-    vec3 result = ambient * inColor; 
+    vec3 result = ambient * inColor.xyz; 
     return result;
 }
 
@@ -42,10 +45,10 @@ vec3 specular(vec3 _viewSpaceNormal)
 
 void main()
 {
-    vec4 texColor = texture(ourTexture, outTexCoords);
+    vec4 texColor = mix(texture(ourTexture, outTexCoords), texture(ourTexture2,outTexCoords),blend);
     vec3 normviewSpaceNormal = normalize(viewSpaceNormal);
 
     vec3 objectColor = diffuse(normviewSpaceNormal) + ambient() + specular(normviewSpaceNormal);
-    vec3 result = objectColor * texColor.xyz;
-    color = vec4(result, 1);
+    //vec3 result = objectColor * texColor.xyz;
+    color = texColor; // + vec4(objectColor,1);//vec4(result, 1);
 }
